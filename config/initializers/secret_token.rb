acf106ac3e6f64271d3ecc9cc0a01555f2ed214c
felipe <felipe@felipe-VirtualBox.(none)>
2012-02-07 10:24:46 -0200

    criar

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Laticinio::Application.config.secret_token = '307429fdd8a978b23b16c15fb2d58dae923083c6443209dfc41999179cbb0aa8b93251237428e96c061fff193c715270c9707a426f3a0efc50ee4de20a3d0729'
